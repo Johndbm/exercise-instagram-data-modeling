6e5fd88c6eb84d5f1a78a85c74d12bc0bb1e8072
john.banks <johndbanksm@gmail.com>
2023-01-27 13:06:19 +0000

    Updated models.py, +diagram.png generated

--- src/models.py
+++ src/models.py
@@ -10,21 +10,23 @@ Base = declarative_base()
 
 class User(Base):
     __tablename__ = 'user'
-    # Here we define columns for the table person
+    # Here we define columns for the table user
     # Notice that each column is also a normal Python instance attribute.
-    id = Column(Integer, primary_key=True)
+    id = Column(Integer, primary_key=True, unique=True, nullable=False)
     username = Column(String(250), nullable=False)
     firstname = Column(String(250), nullable=False)
     lastname = Column(String(250), nullable=False)
-    email = Column(String(250), nullable=False)
+    email = Column(String(250), unique=True)
 
 class Follower(Base):
     __tablename__ = 'follower'
+    id = Column(Integer, primary_key=True)
     user_from_id = Column(Integer, ForeignKey('user.id'))
     user_to_id = Column(Integer, ForeignKey('user.id'))
 
 class Comment(Base):
     __tablename__ = 'comment'
+    id = Column(Integer, primary_key=True)
     comment_text = Column(String(250))
     author_id = Column(Integer, ForeignKey('user.id'))
     post_id = Column(Integer, ForeignKey('post.id'))
@@ -37,7 +39,7 @@ class Post(Base):
 class Media(Base):
     __tablename__ = 'media'
     id = Column(Integer, primary_key=True)
-    type = Column(enum)
+    media_type = Column(String(10))
     url = Column(String(500))
     post_id = Column(Integer, ForeignKey('post.id'))
 
